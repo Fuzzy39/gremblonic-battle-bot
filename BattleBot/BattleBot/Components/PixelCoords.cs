ca660bfe09b844c462ea01b2169a2f4f4276267b
Fuzzy39 <jmcraft126@gmail.com>
2025-01-08 22:02:01 -0500

    Additional testing, bug with ScaledRenderer revealed

--- BattleBot/BattleBot/Components/PixelCoords.cs
+++ BattleBot/BattleBot/Components/PixelCoords.cs
@@ -4,11 +4,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Util.Graphics;
 
 namespace BattleBot.Components
 {
     internal class PixelCoords : EngineCore.Component
     {
-        public Rectangle bounds;
+        public RotatedRect bounds;
     }
 }
--- BattleBot/BattleBot/Main/BattleBotGame.cs
+++ BattleBot/BattleBot/Main/BattleBotGame.cs
@@ -6,6 +6,8 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
+using Util;
+using Util.Graphics;
 
 namespace BattleBot.Main
 {
@@ -25,14 +27,13 @@ namespace BattleBot.Main
         protected override void Initialize()
         {
             Window.AllowUserResizing = true;
+
             engine = new(GraphicsDevice);
             new PixelRenderingSystem(engine);
+            new CircleMovementSystem(engine);
             engine.Initialize();
 
-            
-
-            // that's rather gross.
-
+      
 
             base.Initialize();
            
@@ -45,18 +46,48 @@ namespace BattleBot.Main
             // make an example entity.
             Entity e = new(engine);
 
-            PixelCoords p = new()
+            e.AddComponent(new PixelCoords()
             {
-                bounds = new Rectangle(100, 100, 200, 200)
-            };
-            e.AddComponent(p);
+                bounds = new RotatedRect( new Rectangle(100, 100, 200, 200), 0f, new(200,200))  
+            });
 
-            SimpleTexture st = new()
+            e.AddComponent(new SimpleTexture()
             {
                 texture = assetManager.getTexture(TextureAsset.TestSquare),
                 tint = Color.White
-            };
-            e.AddComponent(st);
+            });
+
+            e.AddComponent(new PointRotation()
+            {
+                center = new Point(300, 300),
+                radius = 200,
+                rotationalVelcocity = 1f,
+                otherRotationVelocity = -.4f
+            });
+
+            e.StopEditing();
+
+
+            e = new(engine);
+
+            e.AddComponent(new PixelCoords()
+            {
+                bounds = new RotatedRect(new Rectangle(100, 100, 100, 500), 1f, new(150, 300))
+            });
+
+            e.AddComponent(new SimpleTexture()
+            {
+                texture = assetManager.getTexture(TextureAsset.TestSquare),
+                tint = Color.White
+            });
+
+            e.AddComponent(new PointRotation()
+            {
+                center = new Point(900, 450),
+                radius = 250,
+                rotationalVelcocity = -1.5f,
+                otherRotationVelocity = 4f
+            });
 
             e.StopEditing();
         }
